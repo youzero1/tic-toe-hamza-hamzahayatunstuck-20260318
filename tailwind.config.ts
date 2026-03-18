29e5464140d3b2e28cde85f3b2295c57e0c5e8f4
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-18 19:04:23 +0500

    Add tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'x-color': '#3B82F6',
+        'o-color': '#EF4444',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
